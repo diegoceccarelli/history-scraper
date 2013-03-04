02862848fc46e83ffb1730cf925b24aeedd63a11
diego <diego.ceccarelli@gmail.com>
2013-03-04 19:31:54 +0100

    remove history dump

--- filter-history.sh
+++ filter-history.sh
@@ -4,6 +4,7 @@ DUMP=history-dump.tsv
 echo "filtering history"
 egrep -f domains.tsv $DUMP | sort -k1 -n > $USER-filtered-history.tsv
 echo "news browsing stored in $USER-filtered-history.tsv"
+rm $DUMP
 
 
 	
