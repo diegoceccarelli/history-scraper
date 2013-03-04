4c4a9e7c8f8e768672867def9e500c05ded213c5
diego <diego.ceccarelli@gmail.com>
2013-03-04 19:10:52 +0100

    added chromium

--- chrome.sh
+++ chrome.sh
@@ -10,3 +10,8 @@ if [ -f  $HOME/.config/google-chrome/Default/History ]
 	then
 	sqlite3 "$HOME/.config/google-chrome/Default/History" "select url,last_visit_time,title from urls" | awk -F'|' '{ print $2"\t"$1"\t"$3 }'
 fi
+
+if [ -f  $HOME/.config/google-chrome/Default/History ]
+	then
+	sqlite3 "$HOME/.config/chromium/Default/History" "select url,last_visit_time,title from urls" | awk -F'|' '{ print $2"\t"$1"\t"$3 }'
+fi
